1541f8de6ac5d472b8de6cc22a2e14d618c5e633
Weligton <weligton0ferreira@gmail.com>
2021-04-05 15:40:57 -0300

    Correções realizadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const productControler = require('../controllers/productControler');
+const productController = require('../controllers/productController');
 
 const router = express.Router();
 
 // ==> Rotas da API
 
-router.get('/products', productControler.index);
-router.get('/products/:id', productControler.showProduct);
-router.post('/products', productControler.createProduct);
-router.put('/products/:id', productControler.updateProduct);
-router.delete('/products/:id', productControler.deleteProduct);
+router.get('/products', productController.index);
+router.get('/products/:id', productController.showProduct);
+router.post('/products', productController.createProduct);
+router.put('/products/:id', productController.updateProduct);
+router.delete('/products/:id', productController.deleteProduct);
 
 module.exports = router;
